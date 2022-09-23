55a6276a180770505a7810c1859b2bd6c5007a29
Bellusci Pierpaolo <bellusci.pierpaolo@gmail.com>
2022-09-23 18:34:20 +0200

    added to course_project publish steps

--- course_project.py
+++ course_project.py
@@ -1,3 +1,4 @@
+from lzma import is_check_supported
 import sys
 import hat
 import mqtt_handler
@@ -8,9 +9,49 @@ import time
 
 
 def main():
-    #PUT HERE YOUR CODE
-    pass
+    sense_hat = hat.hat()
+    timestamp = int(time.time())
 
+    
+    cpu_serial_id = getserial()
+    
+    BROKER_url = "broker.hivemq.com"
+    BROKER_PORT = 1883
+    CLIEND_ID = cpu_serial_id
+
+    mqtt_client = mqtt_handler.mqtt_handler(
+        BROKER_url,
+        BROKER_PORT,
+        CLIEND_ID
+    )
+    
+    mqtt_client.connect()
+    time.sleep(1)
+    is_connected = mqtt_client.check_connection()
+
+    topic_env = f"IoTCourseData/{cpu_serial_id}/Environment"
+    topic_motion = f"IoTCourseData/{cpu_serial_id}/Motion"
+
+    grace_killer = GracefulKiller()
+
+    if is_connected:
+        print("client Connected to Broker !!")
+        while not grace_killer.is_killed():
+            env_data = sense_hat.get_env()
+            env_data["timestamp"] = timestamp
+
+            motion_data = sense_hat.get_movement()
+            motion_data["timestamp"] = timestamp
+            
+            print(f"ENV_DATA: {env_data}")
+            print(f"MOTION_DATA: {motion_data}")
+
+            mqtt_client.publish(topic_env,json.dumps(env_data))
+            mqtt_client.publish(topic_motion,json.dumps(motion_data))
+            
+            time.sleep(1)
+
+        mqtt_client.disconnect()
 
 if __name__ == "__main__":
     main()
